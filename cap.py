095ef0934cc0e6e433d3d4b85b610554f7e54ade
Tab Tu <tabtu@ttxy.org>
2017-07-04 12:45:58 -0400

    fix download haarcascades from opencv

--- cap.py
+++ cap.py
@@ -2,36 +2,36 @@ import numpy as np
 import cv2
 from PIL import Image, ImageDraw
 
-cv2.namedWindow('test')
+cv2.namedWindow('cap')
 cap = cv2.VideoCapture(0)
 success, frame = cap.read()
-color = (0, 0, 0)
-face_cascade = cv2.CascadeClassifier('/data/haarcascades/haarcascade_frontalface_alt.xml')
+#color = (0, 0, 0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 while(success):
     success, frame = cap.read()
-    #ret = True
     #frame = cv2.imread("test1.jpg", 0)
-    size = frame.shape[:2]
-
+    ##size = frame.shape[:2]
+    #cv2.imshow('origin', frame)
     if frame.ndim == 3:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     else:
-        gray = img #if语句：如果img维度为3，说明不是灰度图，先转化为灰度图gray，如果不为3，也就是2，原图就是灰度图
+        gray = img #gray = 2
 
-    #cv2.equalizeHist(gray, gray)
+    #cv2.imshow('gray', gray)
+    cv2.equalizeHist(gray, gray)
 
-    divisor = 8
-    h, w = size
-    minSize = (w//divisor, h//divisor)
+    ##divisor = 8
+    ##h, w = size
+    ##minSize = (w//divisor, h//divisor)
     #faceRects = face_cascade.detectMultiScale(gray, 1.2, 2, cv2.CASCADE_SCALE_IMAGE,minSize)
 
     #rects = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, flags=0, minSize=10, maxSize=50)
     #recvs = face_cascade.detectMultiScale(gray[1.2, scaleFactor[5, minNeighbors[0, flags[10, minSize[50, maxSize]]]]])
-    resul = face_cascade.detectMultiScale(gray[1.2, scaleFactor[5, minNeighbors[0, flags[10, minSize[100, maxSize]]]]])
+    #recvs = face_cascade.detectMultiScale(gray, 1.2, 5, 0)
+    recv = face_cascade.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.3)
 
-    #cv2.imshow('EyesTracking', gray)
-    cv2.imshow('test', gray)
+    cv2.imshow('cap', gray)
     k = cv2.waitKey(60) & 0xff
     if k == 27:
         break
